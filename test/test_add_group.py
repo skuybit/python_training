a42aac4d760d3136c5b3656130e4f65fcc7c7892
skuybit <marina.skuybit@gmail.com>
2016-11-21 22:28:45 -0500

    change_field_value

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,13 +2,13 @@
 from model.group import Group
 
 
-def _test_add_group(app):
+def test_add_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="abc", header="abc", footer="abc"))
     app.session.logout()
 
 
-def _test_add_empty_group(app):
+def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,4 +1,4 @@
-def _test_delete_first_group(app):
+def test_delete_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.delete_first_group()
     app.session.logout()
