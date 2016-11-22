05593a4a3e2fec2c848b4d808db51cb289b3f798
skuybit <marina.skuybit@gmail.com>
2016-11-21 21:24:49 -0500

    Добавлен conftest

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from model.group import Group
+
+
+@pytest.fixture(scope="session")
+def app(request):
+    fixture=Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
--- fixture/group.py
+++ fixture/group.py
@@ -27,6 +27,17 @@ class GroupHelper:
              wd.find_element_by_name("group_footer").send_keys(group.footer)
              # submit group creation
              wd.find_element_by_name("submit").click()
+             self.return_to_group_page()
+
+
+    def delete_first_group(self):
+        wd=self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
 
 
     def return_to_group_page(self):
